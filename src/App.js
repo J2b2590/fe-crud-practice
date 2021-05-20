cd90abe80e125bfa08b61e10f9f236a8ecd191a3
J2b2590 <J2b2590@gmail.com>
2021-05-20 09:25:42 -0500

    made first call using axios and useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  let [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/todos").then((res) => {
+      console.log(res);
+    });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO</h1>
     </div>
   );
 }
