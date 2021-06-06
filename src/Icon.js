5689d1f6c0b92c7cf4fbec9bf206dc493dbae763
J2b2590 <J2b2590@gmail.com>
2021-06-06 10:18:02 -0500

    set counter on example svg

--- src/Icon.js
+++ src/Icon.js
@@ -1,11 +1,36 @@
 import { render } from "@testing-library/react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ReactComponent as Example } from "./example.svg";
 
-function Icons() {
+function Icons(props) {
+  let [count, setCount] = useState(0);
+  let color = "";
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCount(count + 1);
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
+
+  switch (count) {
+    case 1:
+      color = "green";
+      break;
+    case 2:
+      color = "purple";
+      break;
+    case 3:
+      color = "blue";
+      break;
+
+    default:
+      break;
+  }
+
   return (
     <div>
-      <Example fill="purple" stroke="green" />
+      {count}
+      <Example fill={color} stroke="green" />
     </div>
   );
 }
