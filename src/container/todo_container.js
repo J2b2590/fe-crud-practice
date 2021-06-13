4bf957179a563940b382dbe7a53f6a203d3cfea8
J2b2590 <J2b2590@gmail.com>
2021-06-13 12:37:52 -0500

    persisting data submit

--- src/container/todo_container.js
+++ src/container/todo_container.js
@@ -1,27 +1,47 @@
 import React, { useState } from "react";
 import Todos from "../todos";
+import axios from "axios";
 
 function TodoContainer(props) {
   console.log(props, "CONTAINER");
-  let [todo, setTodo] = useState([]);
+  let [text, setTodo] = useState(" ");
 
   const handleChange = (e) => {
     setTodo(e.target.value);
+    // console.log(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userObject = {
+      text,
+    };
+
+    axios
+      .post("http://localhost:3000/todos", userObject)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <div>
       <p>container</p>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Create todo</h1>
         <input
           onChange={handleChange}
-          value={this.state.todo}
+          value={props.todo}
           type="text"
           name="name"
           placeholder="Enter TODO"
         />
+        <button>Enter</button>
       </form>
       {props.todos.map((todo) => {
         // console.log(todo.text, "MAP");
