cf1ccd5043d91b490ead2b0762e3c7237d5f0e85
J2b2590 <J2b2590@gmail.com>
2021-07-11 12:05:53 -0500

    added state to form and updating state with an array of names

--- src/NewForm.js
+++ src/NewForm.js
@@ -5,14 +5,32 @@ class NewForm extends Component {
     super(props);
     this.state = {
       name: "",
+      arrNames: [],
     };
   }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSumbit = (e) => {
+    e.preventDefault();
+    let formData = { name: this.state.name };
+    let dataArray = this.state.arrNames.concat(formData);
+    this.setState({ arrNames: dataArray });
+  };
   render() {
     return (
       <div>
         <h1>New Form component</h1>
-        <form>
-          <input value={this.state.name} />
+        <form onSubmit={(e) => this.handleSumbit(e)}>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <button>enter</button>
         </form>
       </div>
     );
